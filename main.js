de2569d3a24b9090a1594efd8f405e90682d536c
shorpee <mosopewilliamson@gmail.com>
2021-09-28 08:36:12 +0200

    feat: add onClick properties to links and id for DOM elements

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-fetch("./data.json")
-.then(response  => response.json())
-.then(json => console.log(json))
+//an event listener for the DOM after it Loads
+document.addEventListener("DOMContentLoaded", () => {
+
+    //use Fetch API to get data from JSON file
+  function fetchData() {
+    fetch("./data.json")
+    .then((response) => response.json())
+    .then(data => displayTime(data))
+  }
+
+  function displayTime (data) {
+      
+  }
+});
